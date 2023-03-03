68043943a1d562718ec4859b4ea0452c03eecd5e
tumon <tumonkhn@gmail.com>
2023-03-03 18:59:13 +0600

    .

--- app/comps/AddInfo.tsx
+++ app/comps/AddInfo.tsx
@@ -3,15 +3,28 @@ import styles from "./AddInfo.module.css";
 import Container from "./dls/Container";
 import Spinner from "./dls/Spinner";
 import useSWR from "swr";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { User } from "@prisma/client";
 import { fetcher } from "@/lib/swr/fetcher";
+import useDebounce from "./hooks/useDebounce";
 
+const isObjectEmpty = (objectName: any) => {
+  return Object.keys(objectName).length === 0;
+};
+
+const addUnameToDB = () => {
+  console.error("truesfdf");
+};
 // POST REQ
-
 export default function AddInfo({ session }: { session: Session }) {
-  const [user, setUser] = useState<User>();
-  const [unameErr, setunameErr] = useState();
+  const [user, setUser] = useState<User | null>();
+  const [unameErr, setUnameErr] = useState<string>("");
+
+  const DEBOUNCING_PERIOD_MS = 2000;
+  const debouncedSearchQuery = useDebounce<any>(
+    user?.uname,
+    DEBOUNCING_PERIOD_MS
+  );
 
   const { data, error, isLoading } = useSWR("/api/userinfo", () =>
     fetcher("/api/userinfo", {
@@ -25,7 +38,28 @@ export default function AddInfo({ session }: { session: Session }) {
     }
   }, [data]);
 
-  console.log(user);
+  useEffect(() => {
+    if (debouncedSearchQuery) {
+      console.log(debouncedSearchQuery);
+      const get = async () => {
+        fetch("/api/checkuname", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ uname: debouncedSearchQuery }),
+        })
+          .then((r) => r.json())
+          .then((d) => {
+            isObjectEmpty(d) == false
+              ? setUnameErr("Try another username")
+              : setUnameErr("");
+          });
+      };
+      get();
+    }
+  }, [debouncedSearchQuery]);
+
   if (error) return <div>failed to load</div>;
   if (isLoading)
     return (
@@ -35,6 +69,9 @@ export default function AddInfo({ session }: { session: Session }) {
         </div>
       </Container>
     );
+
+  console.log("render");
+  // const unameHadler = (e: FormEvent) => { setUser({...user, uname: "sdfsf"})};
 
   return (
     <Container px1em>
@@ -52,12 +89,17 @@ export default function AddInfo({ session }: { session: Session }) {
           <input
             type="text"
             autoComplete="text"
-            onChange={(e) => null}
+            onChange={(e) => {
+              if (user) setUser({ ...user, uname: e.target.value });
+            }}
             value={user?.uname == null ? "" : user.uname}
             required
             placeholder="Enter your username"
           />
-          <button type="submit">Submit</button>
+          <span> {unameErr} </span>
+          <button style={{ opacity: unameErr ? "20%" : "100%" }} type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </Container>
--- pages/api/updateuname/index.ts
+++ pages/api/updateuname/index.ts
@@ -1,17 +1,20 @@
 import prismaClient from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
-// check uname by uname
 
+// update username by email
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
+  if (req.method === "PUT") {
     try {
-      const data = await prismaClient.user.findMany({
+      const updateUser = await prismaClient.user.update({
         where: {
-          name: req.body.uname,
+          email: req.body.email,
+        },
+        data: {
+          uname: req.body.uame,
         },
       });
 
-      res.status(200).send({ ...data });
+      res.status(200).send("username updated successfully");
     } catch (err) {
       return res.status(404).send({ msg: err });
     }
