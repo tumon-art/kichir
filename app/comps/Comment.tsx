8bdcb4a5843bfa94ff8d393a4190bf530973b7d1
tumon <tumonkhn@gmail.com>
2023-05-08 09:19:15 +0600

    css fixes

--- app/comps/Comment.tsx
+++ app/comps/Comment.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./Comment.module.css";
 import { Session } from "next-auth";
 import { FormEvent, useState } from "react";
+import Container from "./dls/Container";
 
 export default function Comment({
   kichirId,
@@ -33,28 +34,30 @@ export default function Comment({
 
   return (
     <main className={styles.main}>
-      <form onSubmit={onFormSubmit} className={styles.comment}>
-        <div className={styles.AvaterNcommentHold}>
-          <Image
-            src={session?.user?.image || defaultImg}
-            height="30"
-            width="30"
-            alt="img"
-            className={styles.img}
-          />
-          <textarea
-            maxLength={200}
-            className={styles.textArea}
-            placeholder={`Reply Kichri`}
-            value={commentText}
-            onChange={(e) => setCommentText(e.target.value)}
-            required
-          ></textarea>
-        </div>
-        <button type="submit" className={styles.Btn}>
-          Reply
-        </button>
-      </form>
+      <Container px1em>
+        <form onSubmit={onFormSubmit} className={styles.comment}>
+          <div className={styles.AvaterNcommentHold}>
+            <Image
+              src={session?.user?.image || defaultImg}
+              height="30"
+              width="30"
+              alt="img"
+              className={styles.img}
+            />
+            <textarea
+              maxLength={200}
+              className={styles.textArea}
+              placeholder={`Reply Kichri`}
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button type="submit" className={styles.Btn}>
+            Reply
+          </button>
+        </form>
+      </Container>
     </main>
   );
 }
--- app/comps/KichirComp.tsx
+++ app/comps/KichirComp.tsx
@@ -15,6 +15,7 @@ import { AllKichris } from "./ShowKichirs";
 import Comment from "./Comment";
 import useSWR from "swr";
 import ShowComments from "./ShowComments";
+import Card from "./dls/Card";
 
 export default function KichirComp({ kichir }: { kichir: AllKichris }) {
   console.log("Kichir Comp");
@@ -77,169 +78,171 @@ export default function KichirComp({ kichir }: { kichir: AllKichris }) {
   };
 
   return (
-    <Container key={kichir.id} px1em>
-      <div className={styles.card}>
-        <div className={styles.cardSect2}>
-          {/* --- CARD TOP HEAD */}
-          <div className={styles.cardTop}>
-            <div className={styles.cardTopSect1}>
-              <div className={styles.imgHoldMobile}>
-                <Image
-                  src={kichir.author.image || defaultImg}
-                  alt="author"
-                  height={40}
-                  width={40}
-                  className={styles.authorImg}
-                />
-              </div>
-              <Link href={`kichir/name`}>
-                <span className={styles.name}> {kichir.author.name} </span>
-              </Link>
-              <span className={styles.uname}> @{kichir.author.uname} </span>
-              <span className={styles.timehold}>
-                <span className={styles.dot}></span>
-                <span className={styles.time}>
-                  {dayjs(kichir.createdAt).fromNow()}
+    <>
+      <Container key={kichir.id} px1em>
+        <div className={styles.card}>
+          <div className={styles.cardSect2}>
+            {/* --- CARD TOP HEAD */}
+            <div className={styles.cardTop}>
+              <div className={styles.cardTopSect1}>
+                <div className={styles.imgHoldMobile}>
+                  <Image
+                    src={kichir.author.image || defaultImg}
+                    alt="author"
+                    height={40}
+                    width={40}
+                    className={styles.authorImg}
+                  />
+                </div>
+                <Link href={`kichir/name`}>
+                  <span className={styles.name}> {kichir.author.name} </span>
+                </Link>
+                <span className={styles.uname}> @{kichir.author.uname} </span>
+                <span className={styles.timehold}>
+                  <span className={styles.dot}></span>
+                  <span className={styles.time}>
+                    {dayjs(kichir.createdAt).fromNow()}
+                  </span>
                 </span>
+              </div>
+              <span
+                className={styles.ellipsisIconHold}
+                onClick={() => {
+                  if (session?.user?.id === kichir.authorId)
+                    setSelectedElement(kichir);
+                }}
+              >
+                <Ellipsis />
               </span>
             </div>
-            <span
-              className={styles.ellipsisIconHold}
-              onClick={() => {
-                if (session?.user?.id === kichir.authorId)
-                  setSelectedElement(kichir);
-              }}
-            >
-              <Ellipsis />
-            </span>
-          </div>
 
-          {/* --- CARD BODY TEXT */}
-          <div className={styles.bodyText}>{kichir.body}</div>
+            {/* --- CARD BODY TEXT */}
+            <div className={styles.bodyText}>{kichir.body}</div>
 
-          {/* --- CARD IMAGE */}
-          {kichir.img && (
-            <div className={styles.kichirImageHold}>
-              <Image
-                src={kichir.img}
-                height="360"
-                width="440"
-                alt="Image"
-                className={styles.kichirImage}
-                onClick={() => setImageModal(kichir.img!)}
-              />
-            </div>
-          )}
+            {/* --- CARD IMAGE */}
+            {kichir.img && (
+              <div className={styles.kichirImageHold}>
+                <Image
+                  src={kichir.img}
+                  height="360"
+                  width="440"
+                  alt="Image"
+                  className={styles.kichirImage}
+                  onClick={() => setImageModal(kichir.img!)}
+                />
+              </div>
+            )}
 
-          {/* --- CARD FOOTER */}
-          <div className={styles.cardFooter}>
-            <div
-              className={styles.iconHold}
-              onClick={() => {
-                handleLove(kichir.id, String(session?.user?.id));
-                if (isLiked) kichir.loves.pop();
-                else
-                  kichir.loves.push({
-                    id: "demo",
-                    createdAt: new Date(),
-                    kichirId: 123,
-                    userId: "333",
-                  });
-              }}
-            >
-              <Love
-                cssStyles={`${styles.loveIcon} ${isLiked && styles.loveFilled}`}
-              />
-              <span> {kichir.loves.length}</span>
-            </div>
+            {/* --- CARD FOOTER */}
+            <div className={styles.cardFooter}>
+              <div
+                className={styles.iconHold}
+                onClick={() => {
+                  handleLove(kichir.id, String(session?.user?.id));
+                  if (isLiked) kichir.loves.pop();
+                  else
+                    kichir.loves.push({
+                      id: "demo",
+                      createdAt: new Date(),
+                      kichirId: 123,
+                      userId: "333",
+                    });
+                }}
+              >
+                <Love
+                  cssStyles={`${styles.loveIcon} ${
+                    isLiked && styles.loveFilled
+                  }`}
+                />
+                <span> {kichir.loves.length}</span>
+              </div>
 
-            <div className={styles.iconHold}>
-              <CommentIcon cssStyles={styles.loveIcon} />
-              <span> {randomInt(1, 10)} </span>
-            </div>
-            <div className={styles.iconHold}>
-              <View cssStyles={styles.ViewIcon} />
-              <span> {randomInt(50, 200)} </span>
+              <div className={styles.iconHold}>
+                <CommentIcon cssStyles={styles.loveIcon} />
+                <span> {randomInt(1, 10)} </span>
+              </div>
+              <div className={styles.iconHold}>
+                <View cssStyles={styles.ViewIcon} />
+                <span> {randomInt(50, 200)} </span>
+              </div>
             </div>
           </div>
-
-          {/* COMMENT COMPONENT */}
-          <Comment
-            kichirId={kichir.id}
-            session={session}
-            defaultImg={defaultImg}
-          />
         </div>
-      </div>
+
+        {/* --- DELETE KICHIR MODAL */}
+        {selectedElement ? (
+          <Modal cssStyles={styles.customModal} setModel={setSelectedElement}>
+            <h2> Wanna Delete This Kichir? </h2>
+            <div className={styles.askHold}>
+              <div
+                onClick={async () => {
+                  setSelectedElement(undefined);
+                  // mutateKichir(deleteKichir, {
+                  //   optimisticData: () =>
+                  //     allKichir.filter(
+                  //       (kichir) => kichir.id !== selectedElement.id
+                  //     ),
+                  //   populateCache: () =>
+                  //     allKichir.filter(
+                  //       (kichir) => kichir.id !== selectedElement.id
+                  //     ),
+                  //   revalidate: false,
+                  // });
+                  function deleteKichir() {
+                    return fetch(
+                      `/api/deletekichir?id=${selectedElement?.id}`,
+                      {
+                        method: "DELETE",
+                      }
+                    )
+                      .then((response) => {
+                        if (!response.ok) {
+                          throw new Error("Network response was not ok");
+                        }
+                        toast("Kichir deleted successfully");
+                        return response.json();
+                      })
+                      .catch((error) => {
+                        console.error("Error deleting Kichir:", error);
+                      });
+                  }
+                }}
+              >
+                Yes
+              </div>
+              <div
+                onClick={() => {
+                  setSelectedElement(undefined);
+                }}
+              >
+                No
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+
+        {/* --- IMAGE MODAL  */}
+        {imageModal && (
+          <ImageModal setImageModel={setImageModal}>
+            {/* --- X button for Preveiw */}
+            <span onClick={() => setImageModal(undefined)}>
+              <XMark cssStyles={styles.xMarkIcon} />
+            </span>
+            <Image
+              src={imageModal}
+              height="360"
+              width="490"
+              alt="Image"
+              className={styles.imageModal}
+            />
+          </ImageModal>
+        )}
+      </Container>
+      {/* COMMENT COMPONENT */}
+      <Comment kichirId={kichir.id} session={session} defaultImg={defaultImg} />
 
       {/* SHOW ALL COMMENTS */}
       <ShowComments kichirId={kichir.id} />
-
-      {/* --- DELETE KICHIR MODAL */}
-      {selectedElement ? (
-        <Modal cssStyles={styles.customModal} setModel={setSelectedElement}>
-          <h2> Wanna Delete This Kichir? </h2>
-          <div className={styles.askHold}>
-            <div
-              onClick={async () => {
-                setSelectedElement(undefined);
-                // mutateKichir(deleteKichir, {
-                //   optimisticData: () =>
-                //     allKichir.filter(
-                //       (kichir) => kichir.id !== selectedElement.id
-                //     ),
-                //   populateCache: () =>
-                //     allKichir.filter(
-                //       (kichir) => kichir.id !== selectedElement.id
-                //     ),
-                //   revalidate: false,
-                // });
-                function deleteKichir() {
-                  return fetch(`/api/deletekichir?id=${selectedElement?.id}`, {
-                    method: "DELETE",
-                  })
-                    .then((response) => {
-                      if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                      }
-                      toast("Kichir deleted successfully");
-                      return response.json();
-                    })
-                    .catch((error) => {
-                      console.error("Error deleting Kichir:", error);
-                    });
-                }
-              }}
-            >
-              Yes
-            </div>
-            <div
-              onClick={() => {
-                setSelectedElement(undefined);
-              }}
-            >
-              No
-            </div>
-          </div>
-        </Modal>
-      ) : null}
-
-      {/* --- IMAGE MODAL  */}
-      {imageModal && (
-        <ImageModal setImageModel={setImageModal}>
-          {/* --- X button for Preveiw */}
-          <span onClick={() => setImageModal(undefined)}>
-            <XMark cssStyles={styles.xMarkIcon} />
-          </span>
-          <Image
-            src={imageModal}
-            height="360"
-            width="490"
-            alt="Image"
-            className={styles.imageModal}
-          />
-        </ImageModal>
-      )}
-    </Container>
+    </>
   );
 }
